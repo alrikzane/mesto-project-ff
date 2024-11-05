4806c602ea608ecdc4258097f2d7d8010e048c13
Aleksei Zakharchenko <ckill071@gmail.com>
2024-11-05 05:55:35 +0300

    remove todo

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,3 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
 const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
 const galleryList = document.querySelector(".places__list");
 
